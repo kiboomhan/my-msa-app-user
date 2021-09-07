a6ace9fb9e6eb020ec6c73c7bd432662d9ad5185
kiboomhan <kiboomhan@gmail.com>
2021-09-07 15:14:32 +0900

    사용자 인증/인가 관련 구현

--- src/main/java/com/example/msaappuser/config/security/WebSecurity.java
+++ src/main/java/com/example/msaappuser/config/security/WebSecurity.java
@@ -1,24 +1,48 @@
 package com.example.msaappuser.config.security;
 
+import com.example.msaappuser.service.UserService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+    private Environment env;
+    private UserService userService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
-        http.headers().frameOptions().disable(); // h2-console 활용하기 위함
+    public WebSecurity(Environment env, UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.env = env;
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+    protected void configure(HttpSecurity http) throws Exception { // 권한 관련
+        http.csrf().disable();
+//        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+                .hasIpAddress("192.168.10.10")
+                .and()
+                .addFilter(getAuthenticationFilter());
+
+        http.headers().frameOptions().disable(); // h2-console 활용하기 위함
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManager(), userService, env);
+//        authenticationFilter.setAuthenticationManager(authenticationManager()); // 생성자로 변경
+
+        return authenticationFilter;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception { // 인증 관련
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
     }
 }
---
+++ src/main/java/com/example/msaappuser/vo/RequestLogin.java
@@ -0,0 +1,16 @@
+package com.example.msaappuser.vo;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+public class RequestLogin {
+    @NotNull(message = "Email cannot be null")
+    @Size(min = 2, message = "Email not be less than 2 characters")
+    private String email;
+    @NotNull(message = "Password cannot be null")
+    @Size(min = 8, message = "Password must be equals or grater than 8 characters")
+    private String password;
+}
